9939c606ba26a5466eb2fb68a20490642c7d0f3d
Sopan M. Sanap <globallogic@NAG1-LMC-N07230.local>
2022-10-03 19:53:46 +0530

    user name set correctly

--- BOPIS/BOPIS/Extension/IntExtension.swift
+++ BOPIS/BOPIS/Extension/IntExtension.swift
@@ -2,7 +2,7 @@
 //  IntExtension.swift
 //  BOPIS
 //
-//  Created by GlobalLogic on 03/10/22.
+//  Created by Sopan M. Sanap on 03/10/22.
 //
 
 import Foundation
--- BOPIS/BOPIS/Utility/Constant.swift
+++ BOPIS/BOPIS/Utility/Constant.swift
@@ -2,7 +2,7 @@
 //  Constant.swift
 //  BOPIS
 //
-//  Created by GlobalLogic on 03/10/22.
+//  Created by Sopan M. Sanap on 03/10/22.
 //
 
 import Foundation
