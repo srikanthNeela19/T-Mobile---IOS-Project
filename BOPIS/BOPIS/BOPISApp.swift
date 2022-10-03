958d16a9cca6a2c7a8bd239a36fd87b45e9373b7
Ganesh Ubale <ganesh.ubale@globallogic.com>
2022-10-03 17:24:37 +0530

    Blank Project Commit

---
+++ BOPIS/BOPIS/BOPISApp.swift
@@ -0,0 +1,17 @@
+//
+//  BOPISApp.swift
+//  BOPIS
+//
+//  Created by Ganesh Ubale on 03/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BOPISApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
